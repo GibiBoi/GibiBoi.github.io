dd7537157ed159d1893dc87ec2438e3825016f84
GibiBoi <42900705+GibiBoi@users.noreply.github.com>
2019-02-08 22:14:55 +0000

    updated passw

--- sitejava.js
+++ sitejava.js
@@ -1,6 +1,6 @@
 function getpassw()
 	{
 	var pattempt = prompt("Enter Leader Password");
-	if (Math.log(pattempt)/Math.log(8)==15) 
+	if (Math.log(pattempt)/Math.log(8)==5) 
 		{window.location.href="kahHE87EGEg76.html";}
 }
