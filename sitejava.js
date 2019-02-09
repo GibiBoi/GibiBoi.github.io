89b803cb80b804d8e2c7c154eca85683431c0dc4
GibiBoi <42900705+GibiBoi@users.noreply.github.com>
2019-02-09 02:58:44 +0000

    added music

--- sitejava.js
+++ sitejava.js
@@ -4,3 +4,15 @@ function getpassw()
 	if (Math.log(pattempt)/Math.log(8)==5) 
 		{window.location.href="kahHE87EGEg76.html";}
 }
+function music(song)
+{
+	var audio = new Audio(song);audio.play();
+}
+var ian = 0;
+function allian(graphic)
+{
+	if (ian==0) {
+		graphic.src='ian.jpg';ian=1;}
+	else {
+		graphic.src='g photo.jpg';ian=0;}
+}
