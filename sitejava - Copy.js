46f871596cc79ef4bbc0f188d8e9d9e82d8d8c18
GibiBoi <42900705+GibiBoi@users.noreply.github.com>
2019-03-06 18:20:38 +0000

    Mailing attempt

--- sitejava - Copy.js
+++ sitejava - Copy.js
@@ -1,4 +1,4 @@
-/*var downable = 0;
+var downable = 0;
 function getpassw(pass=0)
 {
 	
@@ -29,7 +29,7 @@ function startcomp(pass=0)
 {
 	if (pass) {window.location.href="comp.html";return 0;}
 	var pattempt = prompt("Enter competition password");
-	if (pattempt.toLowerCase()=="showUtility")
+	if (pattempt.toLowerCase()=="showutility")
 	{
 		window.location.href="comp.html";
 	}
@@ -53,5 +53,4 @@ function unhidecomp()
 function filedown(patha)
 {
 	if (downable) {alert("The next area isn't finished yet. You'll be told when the next part is ready :)")} //{window.location.href=patha;}
-}*/
-var _0x363e=['toLowerCase','carrot','style','#555555','backgroundColor','getElementById','comphid','visibility:visible','The\x20next\x20area\x20isn\x27t\x20finished\x20yet.\x20You\x27ll\x20be\x20told\x20when\x20the\x20next\x20part\x20is\x20ready\x20:)','location','href','kahHE87EGEg76.html','Enter\x20Leader\x20Password','log','VO/SC\x20web\x20inc\x20passw.wav','play','src','g\x20photo.jpg','winxp.mp3','comp.html'];(function(_0x152acc,_0x49f778){var _0x1300ba=function(_0x362fa8){while(--_0x362fa8){_0x152acc['push'](_0x152acc['shift']());}};_0x1300ba(++_0x49f778);}(_0x363e,0x1c1));var _0x4512=function(_0x45658b,_0xa6ed42){_0x45658b=_0x45658b-0x0;var _0x51c273=_0x363e[_0x45658b];return _0x51c273;};var downable=0x0;function getpassw(_0x37225e=0x0){if(_0x37225e){window[_0x4512('0x0')][_0x4512('0x1')]=_0x4512('0x2');return 0x0;}var _0x36f9c0=prompt(_0x4512('0x3'));if(Math[_0x4512('0x4')](0x1440)/Math['log'](_0x36f9c0)==0x2){window[_0x4512('0x0')][_0x4512('0x1')]=_0x4512('0x2');}else{music(_0x4512('0x5'));}}function music(_0x5f2c15){var _0x1b5b31=new Audio(_0x5f2c15);_0x1b5b31[_0x4512('0x6')]();}var ian=0x0;function allian(_0x100d33){if(ian==0x0){_0x100d33[_0x4512('0x7')]='ian.jpg';ian=0x1;}else{_0x100d33[_0x4512('0x7')]=_0x4512('0x8');ian=0x0;}}function welcomecomp(){var _0x1632e9=new Audio(_0x4512('0x9'));_0x1632e9[_0x4512('0x6')]();}function startcomp(_0x1cbd34=0x0){if(_0x1cbd34){window[_0x4512('0x0')][_0x4512('0x1')]=_0x4512('0xa');return 0x0;}var _0x5c1bf7=prompt('Enter\x20competition\x20password');if(_0x5c1bf7[_0x4512('0xb')]()==_0x4512('0xc')){window['location'][_0x4512('0x1')]=_0x4512('0xa');}else{music(_0x4512('0x5'));}}function darken(_0x224622){_0x224622[_0x4512('0xd')]['backgroundColor']=_0x4512('0xe');}function lighten(_0x4ce079){_0x4ce079[_0x4512('0xd')][_0x4512('0xf')]='#ebebeb';}function unhidecomp(){document[_0x4512('0x10')](_0x4512('0x11'))[_0x4512('0xd')]=_0x4512('0x12');downable=0x1;}function filedown(_0x3a7720){if(downable){alert(_0x4512('0x13'));}}
+}
