9117ad503bd07510be57a908f939e6897ce1e204
GibiBoi <42900705+GibiBoi@users.noreply.github.com>
2019-06-03 19:43:28 +0100

    Removed auto noise, and added Edna Mode

--- sitejava - Copy.js
+++ sitejava - Copy.js
@@ -1,4 +1,6 @@
 var downable = 0;
+
+
 function getpassw(pass=0)
 {
 	
@@ -9,10 +11,14 @@ function getpassw(pass=0)
 		{window.location.href="kahHE87EGEg76.html";}
 	else {music('VO/SC web inc passw.wav');}
 }
+
+
 function music(song)
 {
 	var audio = new Audio(song);audio.play();
 }
+
+
 var ian = 0;
 function allian(graphic)
 {
@@ -21,10 +27,24 @@ function allian(graphic)
 	else {
 		graphic.src='g photo.jpg';ian=0;}
 }
+
+
+var Edna = 0;
+function EdnaMode(graphic)
+{
+	if (Edna==0) {
+		graphic.src='No Capes.jpg';Edna=1;}
+	else {
+		graphic.src='ogs.jpg';Edna=0;}
+}
+
+
 function welcomecomp()
 {
 	var audio = new Audio("winxp.mp3");audio.play();
 }
+
+
 function startcomp(pass=0)
 {
 	if (pass) {window.location.href="comp.html";return 0;}
@@ -38,18 +58,26 @@ function startcomp(pass=0)
 		music('VO/SC web inc passw.wav');
 	}
 }
+
+
 function darken(elem)
 {
 	elem.style.backgroundColor='#555555';
 }
+
+
 function lighten(elem)
 {
 	elem.style.backgroundColor='#ebebeb';
 }
+
+
 function unhidecomp()
 {
 	document.getElementById("comphid").style="visibility:visible";downable=1;
 }
+
+
 function filedown(patha)
 {
 	if (downable) {alert("The next area isn't finished yet. You'll be told when the next part is ready :)")} //{window.location.href=patha;}
